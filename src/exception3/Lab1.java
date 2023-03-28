1466abd94589ec0c047dbcd546ef0125b0e22c54
Student <Student@5CD3019B6L.control.ad.sit.kmutt.ac.th>
2023-03-28 14:53:59 +0700

    exceptionPart1

---
+++ src/exception3/Lab1.java
@@ -0,0 +1,18 @@
+package exception3;
+
+
+public class Lab1 {
+    public static void main(String[] args) {
+        //System.out.println(1/0);
+        //int[] list  = new list[5];
+        //System.out.println(list[5]);
+        //String s = "abc";
+        //System.out.println(s.charAt(3));
+        //Object o = new Object();
+        //String s = (String) o;
+        //Object o = null;
+        //o.toString();
+        //System.out.println(1.0/0);
+        throw new ArithmeticException();
+    }
+}
